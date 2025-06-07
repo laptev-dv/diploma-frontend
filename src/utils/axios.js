7cba08d786901415483119460b8b4b089a5c9268
Danila Laptev <po4tastandartnay@gmail.com>
2025-06-07 19:39:07 +0300

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL + process.env.REACT_APP_BACKEND_PORT,
+    baseURL: `${process.env.REACT_APP_BACKEND_URL}:${process.env.REACT_APP_BACKEND_PORT}`,
     withCredentials: true,
     headers: {
         'Accept': 'application/json',
