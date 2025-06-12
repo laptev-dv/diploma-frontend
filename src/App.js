d4ae6f826794e06ceabf53b5b9c66b6c0b950f88
Danila Laptev <po4tastandartnay@gmail.com>
2025-06-12 13:23:50 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 // src/App.js
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
